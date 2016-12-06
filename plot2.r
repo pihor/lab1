7dbf82e58f2ffa71523838a2a51882e4b3fc707f
pihor <ihorpihor@gmail.com>
2016-12-06 18:38:07 +0200

    lr5

---
+++ plot2.r
@@ -0,0 +1,20 @@
+#PLOT 2
+# Data reading
+dataframe <- read.csv("E:/RStudio/lab5/household_power_consumption.txt", header=TRUE, dec=".",sep=";")
+
+# Clear all fields with "?"
+cleanframe <- dataframe[apply(dataframe, 1, function(row) {all(!('?' %in% row))}),]
+
+# Convert factor to numeric
+cleanframe$Global_active_power = as.numeric(levels(cleanframe$Global_active_power))[cleanframe$Global_active_power]
+# Convert factor to Date
+cleanframe$Date = as.Date(cleanframe$Date, format = "%d/%m/%Y")
+
+plotdata <- cleanframe[cleanframe["Date"]=="2007-02-01" | cleanframe["Date"]=="2007-02-02","Global_active_power"]
+
+#Draw plot
+plot(plotdata,type="l",xaxt="n", xlab="", ylab="Global Active Pover (kilowatts)")
+axis(1, at=c(0,length(plotdata)/2,length(plotdata)),labels=c("Thu","Fri","Sat"))
+# Save image
+dev.copy(device = png, filename = 'E:/RStudio/lab5/second.png', width = 480, height = 480)
+dev.off ()
